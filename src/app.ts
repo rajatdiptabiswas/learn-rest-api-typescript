1a8d3c61f51212e1587c3e8e573995dd30739c1c
Rajat Dipta Biswas <rdbiswas97@gmail.com>
2021-05-29 21:16:38 +0600

    Connect to database
    - Use Mongoose to connect to MongoDB database
    - Use dotenv to load environment variables from .env file

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 import express, { Application, Request, Response } from 'express';
+import mongoose from 'mongoose';
 
 const app: Application = express();
 const port: number = 3000;
+const dbURI: string = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@googlecloudcluster.n5v9j.mongodb.net/blogs-db?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) =>
+    app.listen(port, () => console.log(`Sever running on port ${port}...`))
+  )
+  .catch((error) => console.log(error));
 
 app.get('/', (request: Request, response: Response) => {
-  response.send("Hello World!");
+  response.send('Hello World!');
 });
-
-app.listen(port, () => console.log(`Sever running on port ${port}...`));
