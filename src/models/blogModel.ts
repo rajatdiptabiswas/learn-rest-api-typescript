6cb64f77d4879aa5d33e663c421cfbea7f6ba344
Rajat Dipta Biswas <rdbiswas97@gmail.com>
2021-06-14 17:52:06 +0600

    Format blogModel.ts

--- src/models/blogModel.ts
+++ src/models/blogModel.ts
@@ -1,24 +1,27 @@
 import { model, Schema } from 'mongoose';
 
-const blogSchema: Schema = new Schema({
-  title: {
-    type: String,
-    required: true,
+const blogSchema: Schema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    imageURL: {
+      type: String,
+    },
   },
-  author: {
-    type: String,
-    required: true,
-  },
-  content: {
-    type: String,
-    required: true,
-  },
-  imageURL: {
-    type: String,
+  {
+    timestamps: true,
   }
-}, {
-  timestamps: true,
-});
+);
 
 const Blog = model('Blog', blogSchema);
 
